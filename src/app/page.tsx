6d673172a898d383705da16507dccd2ecd991fb2
Eduardo <jocdwardo25@gmail.com>
2023-08-09 22:12:24 -0500

    update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
       <section className='bg-Light_Lightness_white h-[674px]  flex items-center'>
         <RowSection>
           <>
-            <div className='flex flex-col items-center w-1/2 px-8'>
+            <div className='flex flex-col align-start w-1/2 pl-[150px]'>
               <div className='leading-10 flex flex-col gap-3'>
-                <h1 className='text-4x1 text-Light_Lightness_3 text-5xl mt-[27px] mb-[21px] w-4/5'>
+                <h1 className='text-4x1 text-Light_Lightness_3 text-5xl mt-[27px] mb-[21px] w-2/4'>
                   Ya no abandones los estudios por falta de tutor
                 </h1>
                 <p className='text-Light_Lightness_2'>Servicio de tutoria académica personalizada.</p>
@@ -42,8 +42,8 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className='w-1/2 flex justify-end relative'>
-              <Image className='w-4/5 object-fit h-full absolute top-0 bottom-0 right-0' src={ImageLaptop} alt='image laptop' width={579} height={447}/>
+            <div className='w-1/2 flex justify-start relative'>
+              <Image className='w-4/5 object-fit h-full absolute top-0 bottom-0 right-[auto]' src={ImageLaptop} alt='image laptop' width={579} height={447}/>
             </div>
           </>
         </RowSection>
@@ -76,87 +76,109 @@ export default function Home() {
       <section className='bg-Light_Lightness_white  pt-[128px] pb-[96px]'> 
         <RowSection>
           <>
-            <div className='w-1/2'>
+            <div className='w-1/2 flex justify-end relative'>
               <Image src={ImageUpInformation} alt='image laptop' width={536} height={536}/>
             </div>
-            <FeatureText
-            styles='px-[56px] w-1/2'
-            link='Crea'
-            title='Sube tu información fácil'
-            description='Te presentamos la nueva forma de estudiar con resúmenes 
-                          generados por la IA, cuestionarios lúdicos y asistentes virtuales.'
-            buttonContent='Create a Quiz for Free'
-            buttonBg='bg-Light_Lightness_Purple_1'
-            >
-              <>
-                <div>
-                  <span className='text-Light_Lightness_1 text-[16px]'>
-                    <i className='icon-dobleArrow text-Dark_Darkness_black text-[16px] mr-[12px]'></i>Fast.
-                    <p className='text-Light_Lightness_4 text-[16px] pb-[30px] leading-7'>
-                      question formats. Consistently come up with new ideas 
-                      and inspiration, while saving time and effort.
-                    </p>
-                  </span>
-                  <span className='text-Light_Lightness_1 text-[16px]'>
-                    <i className='icon-reload text-Dark_Darkness_black text-[16px] mr-[12px]'></i>Flexible.
-                    <p className='text-Light_Lightness_4 text-[16px] pb-[35px] leading-7'>
-                      in-the-blank style questions. Upload files like PDFs, 
-                      DOCs, and PPTs to create quizzes and exams.
-                    </p>
-                  </span>
-                  <span className='text-Light_Lightness_1 text-[16px]'>
-                    <i className='icon-integrated text-Dark_Darkness_black text-[16px] mr-[12px]'></i>Integrated.
-                    <p className='text-Light_Lightness_4 text-[16px] pb-[35px] leading-7'>
-                      flashcards in a single click. Quizgecko can even 
-                      generate entire quizzes from Youtube videos.
-                    </p>
-                  </span>
-                </div>
-              </>
-            </FeatureText>
+            <div className='w-1/2 px-[32px] flex justify-start relative'>
+              <div>
+                <FeatureText
+                styles='px-[56px] w-1/2'
+                link='Crea'
+                title='Sube tu información fácil'
+                description='Te presentamos la nueva forma de estudiar con resúmenes 
+                              generados por la IA, cuestionarios lúdicos y asistentes virtuales.'
+                >
+                  <>
+                    <div>
+                      <span className='text-Light_Lightness_1 text-[16px]'>
+                        <i className='icon-dobleArrow text-Dark_Darkness_black text-[16px] mr-[12px]'></i>Fast.
+                        <p className='text-Light_Lightness_4 text-[16px] pb-[30px] leading-7'>
+                          question formats. Consistently come up with new ideas 
+                          and inspiration, while saving time and effort.
+                        </p>
+                      </span>
+                      <span className='text-Light_Lightness_1 text-[16px]'>
+                        <i className='icon-reload text-Dark_Darkness_black text-[16px] mr-[12px]'></i>Flexible.
+                        <p className='text-Light_Lightness_4 text-[16px] pb-[35px] leading-7'>
+                          in-the-blank style questions. Upload files like PDFs, 
+                          DOCs, and PPTs to create quizzes and exams.
+                        </p>
+                      </span>
+                      <span className='text-Light_Lightness_1 text-[16px]'>
+                        <i className='icon-integrated text-Dark_Darkness_black text-[16px] mr-[12px]'></i>Integrated.
+                        <p className='text-Light_Lightness_4 text-[16px] pb-[35px] leading-7'>
+                          flashcards in a single click. Quizgecko can even 
+                          generate entire quizzes from Youtube videos.
+                        </p>
+                      </span>
+                    </div>
+                  </>
+                </FeatureText>
+                <ButtonPrimary
+                  style='ml-[56px]'
+                  width
+                  borderRadius
+                  fill
+                  content='Create a Quiz for Free'
+                  bg='bg-Light_Lightness_Purple_0'
+                  fontSize='text-[16px]'
+                />
+              </div>
+            </div>
           </>
         </RowSection>
       </section>
       <section className='bg-Light_Lightness_white pr-[32px] py-[96px]'>
         <RowSection>
           <>
-            <FeatureText
-            styles='w-1/2 px-[32px]'
-            link='Share & Embed'
-            title='Publish with ease'
-            description='Effortlessly share and embed quizzes with your team, students, 
-                          or use them as study aids – get everyone engaged and enhance 
-                          learning experiences in just seconds.'
-            buttonContent='Create a Quiz for Free'
-            buttonBg='bg-Light_Lightness_Purple_1'
-            >
-              <>
-                <div>
-                  <span className='text-Light_Lightness_1 text-[16px]'>
-                    <i className='icon-share text-Dark_Darkness_black text-[16px] mr-[12px]'></i>Share.
-                    <p className='text-Light_Lightness_4 text-[16px] pb-[32px] leading-7'>
-                      Share quizzes effortlessly with students or colleagues.
-                    </p>
-                  </span>
-                  <span className='text-Light_Lightness_1 text-[16px]'>
-                    <i className='icon-cloud text-Dark_Darkness_black text-[16px] mr-[12px]'></i>Export.
-                    <p className='text-Light_Lightness_4 text-[16px] pb-[31px] leading-7'>
-                      Download as text, csv and Aiken format for seamless integration 
-                      into your learning management system or classroom setting
-                    </p>
-                  </span>
-                  <span className='text-Light_Lightness_1 text-[16px]'>
-                    <i className='icon-embed text-Dark_Darkness_black text-[16px] mr-[12px]'></i>Embed.
-                    <p className='text-Light_Lightness_4 text-[16px] pb-[31px] leading-7'>
-                      Add quizzes in your own website, LMS or blog. Perfect for 
-                      lead generation, engaging your users
-                    </p>
-                  </span>
-                </div>
-              </>
-            </FeatureText>
-            <div className='w-1/2 flex justify-end relative'>
-              <Image  src={ImagePublishEasy} alt='image ' width={536} height={536}/>
+            <div className='w-1/2 px-[32px] flex justify-end relative'>
+              <div>
+                <FeatureText
+                styles='w-1/2 px-[32px]'
+                link='Share & Embed'
+                title='Publish with ease'
+                description='Effortlessly share and embed quizzes with your team, students, 
+                              or use them as study aids – get everyone engaged and enhance 
+                              learning experiences in just seconds.'
+                >
+                  <>
+                    <div>
+                      <span className='text-Light_Lightness_1 text-[16px]'>
+                        <i className='icon-share text-Dark_Darkness_black text-[16px] mr-[12px]'></i>Share.
+                        <p className='text-Light_Lightness_4 text-[16px] pb-[32px] leading-7'>
+                          Share quizzes effortlessly with students or colleagues.
+                        </p>
+                      </span>
+                      <span className='text-Light_Lightness_1 text-[16px]'>
+                        <i className='icon-cloud text-Dark_Darkness_black text-[16px] mr-[12px]'></i>Export.
+                        <p className='text-Light_Lightness_4 text-[16px] pb-[31px] leading-7'>
+                          Download as text, csv and Aiken format for seamless integration 
+                          into your learning management system or classroom setting
+                        </p>
+                      </span>
+                      <span className='text-Light_Lightness_1 text-[16px]'>
+                        <i className='icon-embed text-Dark_Darkness_black text-[16px] mr-[12px]'></i>Embed.
+                        <p className='text-Light_Lightness_4 text-[16px] pb-[31px] leading-7'>
+                          Add quizzes in your own website, LMS or blog. Perfect for 
+                          lead generation, engaging your users
+                        </p>
+                      </span>
+                    </div>
+                  </>
+                </FeatureText>
+                <ButtonPrimary
+                  style='ml-[32px]'
+                  width
+                  borderRadius
+                  fill
+                  content='Create a Quiz for Free'
+                  bg='bg-Light_Lightness_Purple_0'
+                  fontSize='text-[16px]'
+                />
+              </div>
+            </div>
+            <div className='w-1/2 flex justify-start relative'>
+              <Image className='ml-[52px]' src={ImagePublishEasy} alt='image' width={536} height={536}/>
             </div>
           </>
         </RowSection>
@@ -164,17 +186,17 @@ export default function Home() {
       <section className='bg-Light_Lightness_white pr-[48px] pt-[112px] pb-[176px]'>
         <RowSection>
           <>
-            <div className='w-1/2'>
+            <div className='w-1/2 flex justify-end relative'>
               <Image src={ImagePerformance} alt='image laptop' width={536} height={536}/>
             </div>
+            <div>
+              <div>
             <FeatureText
             styles='w-1/2 px-[56px]'
             link='Analyze'
             title='Measure performance'
             description='Reports and stats to understand how individuals and groups performed. 
-                          Auto-graded quizzes make marking a thing of the past.'
-            buttonContent='Create a Quiz for Free'
-            buttonBg='bg-Light_Lightness_Purple_1'
+            Auto-graded quizzes make marking a thing of the past.'
             >
               <>
                 <div>
@@ -189,6 +211,17 @@ export default function Home() {
                 </div>
               </>
             </FeatureText>
+            <ButtonPrimary
+              style='ml-[56px]'
+              width
+              borderRadius
+              fill
+              content='Create a Quiz for Free'
+              bg='bg-Light_Lightness_Purple_0'
+              fontSize='text-[16px]'
+            />
+            </div>  
+          </div>
           </>
         </RowSection>
       </section>
